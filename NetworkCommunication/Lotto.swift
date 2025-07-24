f5310f2ee599ddfb771bcd8e62fcb0517228b5d9
a-jb97 <a_jb97@naver.com>
2025-07-25 01:19:53 +0900

    [Feat] Lotto 구조체 생성

---
+++ NetworkCommunication/Lotto.swift
@@ -0,0 +1,20 @@
+//
+//  Lotto.swift
+//  NetworkCommunication
+//
+//  Created by 전민돌 on 7/25/25.
+//
+
+import Foundation
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let drwtNo6: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let bnusNo: Int
+    let drwNo: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo1: Int
+}
